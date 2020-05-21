58ee5a2d62aaa18cd74f7fa3b0f93b12f4a9a1ac
Gianni Antonio Manilia <giannimanilia@gmail.com>
2020-05-21 13:52:45 +0200

    Fixed types and stats layouts

--- app/src/main/java/com/gmaniliapp/pokemonlist/presentation/ui/DetailFragment.kt
+++ app/src/main/java/com/gmaniliapp/pokemonlist/presentation/ui/DetailFragment.kt
@@ -1,14 +1,18 @@
 package com.gmaniliapp.pokemonlist.presentation.ui
 
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.gmaniliapp.pokemonlist.R
 import com.gmaniliapp.pokemonlist.databinding.FragmentDetailBinding
 import com.gmaniliapp.pokemonlist.presentation.adapter.PokemonStatsAdapter
 import com.gmaniliapp.pokemonlist.presentation.adapter.PokemonTypesAdapter
+import com.gmaniliapp.pokemonlist.utils.HorizontalItemDecoration
 import com.gmaniliapp.pokemonlist.viewmodel.DetailViewModel
 import com.gmaniliapp.pokemonlist.viewmodel.DetailViewModelFactory
 
@@ -29,11 +33,47 @@ class DetailFragment : Fragment() {
         binding.viewModel = ViewModelProvider(
             this, viewModelFactory).get(DetailViewModel::class.java)
 
-        // Sets the adapter of the pokemonTypes RecyclerView
+        // Set pokemonTypes RecyclerView
         binding.pokemonTypes.adapter = PokemonTypesAdapter()
+        binding.typesCard.setOnClickListener {
+            TransitionManager.beginDelayedTransition(
+                binding.pokemonTypes,
+                AutoTransition()
+            )
+            if (binding.pokemonTypes.visibility == View.VISIBLE) {
+                binding.pokemonTypes.visibility = View.GONE
+                binding.expandArrowTypes.setImageResource(android.R.drawable.arrow_down_float)
+            }
+            else {
+                binding.pokemonTypes.visibility = View.VISIBLE
+                binding.expandArrowTypes.setImageResource(android.R.drawable.arrow_up_float)
+            }
+        }
+        binding.pokemonTypes.addItemDecoration(
+            HorizontalItemDecoration(
+            resources.getDimension(R.dimen.cardview_margin).toInt())
+        )
 
-        // Sets the adapter of the pokemonStats RecyclerView
+        // Set pokemonStats RecyclerView
         binding.pokemonStats.adapter = PokemonStatsAdapter()
+        binding.statsCard.setOnClickListener {
+            TransitionManager.beginDelayedTransition(
+                binding.pokemonStats,
+                AutoTransition()
+            )
+            if (binding.pokemonStats.visibility == View.VISIBLE) {
+                binding.pokemonStats.visibility = View.GONE
+                binding.expandArrowStats.setImageResource(android.R.drawable.arrow_down_float)
+            }
+            else {
+                binding.pokemonStats.visibility = View.VISIBLE
+                binding.expandArrowStats.setImageResource(android.R.drawable.arrow_up_float)
+            }
+        }
+        binding.pokemonStats.addItemDecoration(
+            HorizontalItemDecoration(
+                resources.getDimension(R.dimen.cardview_margin).toInt())
+        )
 
         return binding.root
     }
